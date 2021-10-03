1001f42a355646e93aaae173a38a22e10923fcdb
TomiPr <tomyprastiyo8@gmail.com>
2021-10-03 18:27:30 +0700

    [add] animal componen to home

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AnimalService } from '../animal.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public animalData: Array<any>;
+  public currentAnimal: any;
+
+  constructor (private animalService: AnimalService) {
+    animalService.get().subscribe((data: any) => this.animalData = data);
+  }
 }
